996bac71a412c290979a2ec313619d1a14f994c0
Jiali Tang <violetjiali@gmail.com>
2019-02-03 16:45:12 -0800

    Jpanel draw lines

---
+++ LineDrawing/LineDrawingTest.java
@@ -0,0 +1,23 @@
+package LineDrawing;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class LineDrawingTest {
+
+    public static void main(String[] args) {
+
+        JFrame application = new JFrame();
+        JButton b = new JButton("resume");
+        LiningPanel panel = new LiningPanel();
+        panel.add(b); 
+        
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        application.add(panel);
+        application.setSize(300, 300);
+        application.setTitle("Lining Art");
+        application.setVisible(true);
+
+          
+    }
+}
