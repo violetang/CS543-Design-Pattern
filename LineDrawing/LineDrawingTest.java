8acb79129b9cdea54ee8116f6c6a06a68d25d64b
Jiali Tang <violetjiali@gmail.com>
2019-02-05 21:58:41 -0800

    resume not working

--- LineDrawing/LineDrawingTest.java
+++ LineDrawing/LineDrawingTest.java
@@ -1,5 +1,13 @@
 package LineDrawing;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -8,14 +16,57 @@ public class LineDrawingTest {
     public static void main(String[] args) {
 
         JFrame application = new JFrame();
-        JButton b = new JButton("resume");
+        JButton button = new JButton("draw");
         LiningPanel panel = new LiningPanel();
-        panel.add(b); 
+        BoxLayout box = new BoxLayout(application.getContentPane(), BoxLayout.PAGE_AXIS);
 
         application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        application.add(panel);
-        application.setSize(300, 300);
+        application.getContentPane().setLayout(box);
+        application.add(button);
+        button.setSize(new Dimension(100, 20));
+        button.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
+        application.setSize(300, 320);
         application.setTitle("Lining Art");
-        application.setVisible(true);   
+        application.setVisible(true);
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(button.getText().equals("draw")){
+                    button.setText("pause");
+                    application.add(panel);
+                    panel.start();
+                }                         
+                //                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(button.getText().equals("pause")){
+                    button.setText("resume");
+                    try {
+                        panel.stop();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(LineDrawingTest.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }                         
+                //                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        button.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(button.getText().equals("resume")){
+                    panel.start();
+                    button.setText("pause");
+                    //panel.stop();
+                    //
+                }                         
+                //                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
+    
     }
 }
--- LineDrawing/LiningPanel.java
+++ LineDrawing/LiningPanel.java
@@ -4,46 +4,97 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-public class LiningPanel extends javax.swing.JPanel {
+public class LiningPanel extends javax.swing.JPanel{// implements Runnable {
+    //private Thread update;
 
-    public LiningPanel() { }
-
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        int w = getWidth();
-        int h = getHeight();
-        
-        double lines = 15.0;
-        
-
-        for(int i = 0; i < lines; i++)
-        {
-            int w2 = (int)((i/lines)*w); 
-            int h2 = (int)((i/lines)*h); 
-
-            Random rand = new Random();
-            float red = rand.nextFloat();
-            float green = rand.nextFloat();
-            float yellow = rand.nextFloat();
-
-            Color myColor = new Color(red,green,yellow);
-
-            //左下角
-            g.drawLine(0,  h2, w2, h);
-            g.setColor(myColor);
-
-            //右上角
-            g.drawLine(w,  h2, w2, 0);
-
-            //右下角
-            g.drawLine(w, h-h2, w2, h);
-           
-            //左上角
-            g.drawLine(0, h-h2, w2, 0);
+    double j = -1;
+    double lines = 15.0;
+    int w = 0;
+    int h = 0;
+    int w2, h2, w3, h3;
+    // Thread thread = null;
+    private Graphics G;
+    Thread p = null;
+    boolean run = false;
     
+    public LiningPanel() {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        System.out.println(j + "w2 " + w2);
+        w = getWidth();
+        h = getHeight();
+       
+        for (int i = 0; i <= j; i++) {
+            w2 = (int) ((i / lines) * w); // from 0 to w
+            h2 = (int) ((i / lines) * h); // from 0 to h
+            w3 = (int) (((lines - i) / lines) * w); // from w to 0
+            h3 = (int) (((lines - i) / lines) * h); // from h to 0
+            setNewColor(g);
+            g.drawLine(0, h2, w2, h); // left side to bottom side
+            g.drawLine(0, h2, w3, 0); // up side to left side
+            g.drawLine(w2, h, w, h3); // bottom side to right side
+            g.drawLine(w3, 0, w, h3); // right side to up side
         }
-        
+
+    }
+
+   /* @Override
+    public void run() {
+        while (j < lines) {
+            j++;
+            try {
+                Thread.sleep(500);
+            } catch (Exception ex) {
+                ex.getStackTrace();
+            }
+            repaint();
+        }
+
+    }*/
+
+    public void stop() throws InterruptedException {
+       p.stop();
     }
     
+    public void resume(){
+        p.start();
+    }
+    
+    public void update(Graphics g){
+        paintComponent(g);
+    }
+    public void start() {
+        p = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (j < lines) {
+                    j = (j+1)%lines;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    repaint();
+                }
+            }
+        });
+        p.start();
+    }
+
+    //Method to set random new color for next lines
+
+    public void setNewColor(Graphics g) {
+        Random rand = new Random();
+        int R = rand.nextInt(255);
+        int G = rand.nextInt(255);
+        int B = rand.nextInt(255);
+        Color c = new Color(R, G, B);
+        g.setColor(c);
+    }
+
 }
