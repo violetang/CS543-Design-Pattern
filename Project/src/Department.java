fdbe1c8d01598dd267b89abc3adf268aa9714be2
Jiali Tang <violetjiali@gmail.com>
2019-05-10 21:36:58 -0700

    more function for department

--- Project/src/Department.java
+++ Project/src/Department.java
@@ -3,23 +3,52 @@ import java.util.ArrayList;
 public class Department {
 
     private String name;
+    private Leader leader;
     private ArrayList<Manager> managers;
+    private ArrayList<Worker> workers;
 
-    public Department(String name) {
+    public Department(String name, Leader lead) {
+        this.leader = lead;
         this.name = name;
+        managers = new ArrayList<>();
+        workers = new ArrayList<>();
     }
 
+    //add a manager to this team
     public void addManager(Manager m) {
         this.managers.add(m);
     }
 
+    //add a worker to this team, also assign the worker to a manager
     public void addWorker(Worker w, Manager m) {
         if (this.managers.contains(m)) {
             m.addWorker(w);
+            workers.add(w);
         }
     }
+
+    //get who's leader
+    public Leader getLeader(){
+        return leader;
+    }
+
+    //get department name
+    public String returnDepartmentName(){
+        return name;
+    }
+
+    public ArrayList<Manager> getManagers(){
+        return managers;
+    }
+
+    public ArrayList<Worker> getWorkers(){
+        return workers;
+    }
+
+    /*
     public void setTask(Case c){
         
     }
+    */
     
 }
